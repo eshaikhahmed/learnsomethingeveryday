1fa521f804b1ee877cd44254084f33c6fa4784b9
Ejaz Shaikh <ejaz.shaikh151992@gmail.com>
2022-01-24 00:53:17 +0530

    Added blogs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component }  from 'react';
+import Blogs from './components/Blogs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="header">
+        <div className="menu-bar">
+            <span><a href="index.php">Home</a></span>
+            <span><a href="me.php">Me</a></span>
+        </div>
+      </nav>
+      <Blogs />
     </div>
   );
 }
